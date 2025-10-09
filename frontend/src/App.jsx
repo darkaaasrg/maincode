eb69415bfab82d40680e54646d14fa2d6d3695cf
Daryna <kisildarina09@gmail.com>
2025-10-09 10:43:38 +0300

    нарешті футер працює бож

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+// Припускаємо, що шляхи імпорту коректні
 import Home from "./components/Home";
 import Vinyls from "./Vinyls";
 import Cassettes from "./Cassettes";
@@ -10,19 +11,24 @@ import './index.css'
 function App() {
   return (
     <Router>
-      <Header />
-      
+      {/* 🟢 КРОК 1: Додаємо головний Flex-контейнер */}
+      <div className="app-layout-wrapper"> 
+        
+        <Header />
+        
+        {/* 🟢 КРОК 2: Контейнер для основного вмісту, який має розтягуватися */}
+        <div className="main-content-area">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/vinyls" element={<Vinyls />} />
+            <Route path="/cassettes" element={<Cassettes />} />
+          </Routes>
+        </div>
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/vinyls" element={<Vinyls />} />
-        <Route path="/cassettes" element={<Cassettes />} />
-      </Routes>
-
-      <Footer class="footer" />
+        {/* КРОК 3: Використовуємо className, а не class */}
+        <Footer className="footer" /> 
+      </div>
     </Router>
-
-    
   );
 }
 
